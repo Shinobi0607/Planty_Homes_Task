cd50bb77d727dfb87ffa6eab94ab0087e23294d4
Gaurav Kumar Singh <b210010@nitsikkimm.ac.in>
2023-11-01 23:22:48 +0530

    progress report 1

--- lib/main.dart
+++ lib/main.dart
@@ -30,40 +30,90 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  int selected = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: appBar(),
-        body: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey.shade300,
-                    prefixIcon: IconButton(
-                      iconSize: 30,
-                      color: Colors.green,
-                      icon: const Icon(Icons.search),
-                      onPressed: () {},
-                    ),
-                    suffixIcon: IconButton(
-                      iconSize: 30,
-                      color: Colors.green,
-                      icon: const Icon(Icons.mic),
-                      onPressed: () {},
-                    ),
-                    border: InputBorder.none,
+      appBar: appBar(),
+      body: Column(
+        children: [
+          search_field(),
+          const SizedBox(height: 20),
+          CategorySelector(
+            selected: selected,
+            categories: const ['Sort', 'Gifts', 'Fast Delivery', 'Ceremic'],
+            onTap: (index) {
+              setState(() {
+                selected = index;
+              });
+            },
+          ),
+          const SizedBox(height: 10),
+          Column(
+            children: [
+              const Text(
+                "What's New",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                height: 180,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/gaurav.png'),
                   ),
                 ),
               ),
-            )
-          ],
-        ));
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container search_field() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.grey.shade300,
+            hintText: 'Try searching a nursery or a plant',
+            hintStyle: TextStyle(
+                fontSize: 16,
+                color: Colors.grey.shade500,
+                fontWeight: FontWeight.normal),
+            prefixIcon: IconButton(
+              iconSize: 30,
+              color: Colors.green,
+              icon: const Icon(Icons.search),
+              onPressed: () {},
+            ),
+            suffixIcon: IconButton(
+              iconSize: 30,
+              color: Colors.green,
+              icon: const Icon(Icons.mic),
+              onPressed: () {},
+            ),
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+    );
   }
 
   AppBar appBar() {
@@ -118,6 +168,63 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class CategorySelector extends StatelessWidget {
+  const CategorySelector({
+    Key? key,
+    required this.selected,
+    required this.categories,
+    required this.onTap,
+  }) : super(key: key);
+
+  final int selected;
+  final List<String> categories;
+  final Function(int) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 34.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal, // Enable horizontal scrolling
+        children: [
+          for (int i = 0; i < categories.length; i++)
+            GestureDetector(
+              onTap: () {
+                onTap(i);
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                margin: const EdgeInsets.only(right: 6.0, left: 6.0),
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade300,
+                      blurRadius: 40,
+                      spreadRadius: 3,
+                    )
+                  ],
+                  color: selected == i ? Colors.green : Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Center(
+                  child: Text(
+                    categories[i],
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15.0,
+                      color: selected == i ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
